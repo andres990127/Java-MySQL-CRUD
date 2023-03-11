7fe6f427f1f7ea93515f3ceaaac6a37fe4b01a52
Andrés Mauricio Gutiérrez Rojas <andres_mau.gutierrez@uao.edu.co>
2023-03-11 13:16:52 -0500

    Leyendo mensajes en MySQL

--- src/MensajesDAO.java
+++ src/MensajesDAO.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MensajesDAO {
@@ -25,7 +26,31 @@ public class MensajesDAO {
     }
 
     public static void leerMensajesDB(){
+        Conexion db_connect = new Conexion();
+        try{
+            Connection myConextion = db_connect.getConnection();
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            try{
+                String query = "SELECT * FROM mensajes";
+                ps = myConextion.prepareStatement(query);
+                rs = ps.executeQuery();
 
+                while (rs.next()){
+                    System.out.println("------------------------------------------");
+                    System.out.println("ID: " + rs.getInt("ID_MENSAJE"));
+                    System.out.println("Mensaje: " + rs.getString("MENSAJE"));
+                    System.out.println("Autor: " + rs.getString("AUTOR_MENSAJE"));
+                    System.out.println("Fecha: " + rs.getDate("FECHA_MENSAJE"));
+                    System.out.println("------------------------------------------");
+                }
+            }catch (SQLException e){
+                System.out.println("No se pudieron recuperar los mensajes");
+                System.out.println(e);
+            }
+        }catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     public static void borrarMensajeDB(int id_mensaje){
